37c8cb5d928b8fc4559fe56e2c49d59b7eda4ced
Michael Adrian <michael.adrian@xpwales.com>
2016-06-10 23:58:35 +0100

    Initial

--- src/application/module/XpwCongruence/src/XpwCongruence/Directory/DirectoryEntity.php
+++ src/application/module/XpwCongruence/src/XpwCongruence/Directory/DirectoryEntity.php
@@ -2,12 +2,20 @@
 
 namespace XpwCongruence\Directory;
 
+use Xpwales\Identity\Identity\IdentityAwareAccessorInterface;
 use Xpwales\Identity\Identity\IdentityAwareTrait;
+use XpwCongruence\EnabledStatus\EnabledStatusAwareInterface;
 use XpwCongruence\EnabledStatus\EnabledStatusAwareTrait;
+use XpwCongruence\EntityName\EntityNameAwareInterface;
 use XpwCongruence\EntityName\EntityNameAwareTrait;
+use XpwCongruence\IdHash\IdHashAwareInterface;
 use XpwCongruence\IdHash\IdHashAwareTrait;
 
 class DirectoryEntity
+    implements IdentityAwareAccessorInterface,
+                IdHashAwareInterface,
+                EnabledStatusAwareInterface,
+                EntityNameAwareInterface
 {
     use IdentityAwareTrait,
         IdHashAwareTrait,
