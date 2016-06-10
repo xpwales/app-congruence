d71ad902fc83bb75d2e889f9627ccbd45fa8d1b6
Michael Adrian <michael.adrian@xpwales.com>
2016-06-10 23:07:18 +0100

    Initial

--- src/application/module/XpwCongruence/src/XpwCongruence/Tenant/TenantEntity.php
+++ src/application/module/XpwCongruence/src/XpwCongruence/Tenant/TenantEntity.php
@@ -2,6 +2,7 @@
 
 namespace XpwCongruence\Tenant;
 
+use XpwCongruence\EntityName\EntityNameAwareTrait;
 use XpwCongruence\IdHash\IdHashAwareTrait;
 use XpwCongruence\NameKey\NameKeyAwareTrait;
 use XpwCongruence\Timestamp\EntityTimestampTrait;
@@ -10,6 +11,39 @@ class TenantEntity
 {
     use EntityTimestampTrait,
         IdHashAwareTrait,
-        NameKeyAwareTrait;
+        NameKeyAwareTrait,
+        EntityNameAwareTrait;
+
+    /**
+     * @var null|string
+     */
+    private $sysAdminNotes = null;
+
+    /**
+     * @param string|null $notes
+     *
+     * @return string
+     */
+    public function setSysAdminNotes($notes)
+    {
+        $notes = (string) $notes;
+
+        if (empty($notes) === true) {
+            $this->sysAdminNotes = null;
+
+        } else {
+            $this->sysAdminNotes = $notes;
+        }
+
+        return $notes;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getSysAdminNotes()
+    {
+        return $this->sysAdminNotes;
+    }
 
 }//end class
