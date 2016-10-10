0b3bfe50f573d8b5a7afe6ed05d79bbdd2172440
Michael Adrian <michael.adrian@xpwales.com>
2016-10-10 12:55:56 +0100

    Initial

--- src/application/module/XpwCongruence/src/XpwCongruence/Tenant/Factory/TenantEntityFactory.php
+++ src/application/module/XpwCongruence/src/XpwCongruence/Tenant/Factory/TenantEntityFactory.php
@@ -2,16 +2,28 @@
 
 namespace XpwCongruence\Tenant\Factory;
 
+use Xpwales\Identity\Factory\IdentityFactoryAwareInterface;
+use Xpwales\Identity\Factory\IdentityFactoryAwareTrait;
 use XpwCongruence\Tenant\TenantEntity;
 
-class TenantEntityFactory implements TenantEntityFactoryInterface
+class TenantEntityFactory
+    implements
+        TenantEntityFactoryInterface,
+        IdentityFactoryAwareInterface
 {
+    use IdentityFactoryAwareTrait;
+
     /**
      * @inheritdoc
      */
     public function create()
     {
-        $tenant = new TenantEntity();
+        $tenant          = new TenantEntity();
+        $identityFactory = $this->getIdentityFactory();
+
+        $tenant->setCreatedTimestamp(new \DateTime('now', new \DateTimeZone('UTC')))
+               ->setModifiedTimestamp(new \DateTime('now', new \DateTimeZone('UTC')))
+               ->setIdentity($identityFactory->createIdentity());
 
         return $tenant;
     }
--- src/application/module/XpwCongruence/src/XpwCongruence/Tenant/Service/TenantEntityFactoryServiceFactory.php
+++ src/application/module/XpwCongruence/src/XpwCongruence/Tenant/Service/TenantEntityFactoryServiceFactory.php
@@ -2,6 +2,7 @@
 
 namespace XpwCongruence\Tenant\Service;
 
+use Xpwales\Identity\Factory\IdentityFactoryInterface;
 use XpwCongruence\Tenant\Factory\TenantEntityFactory;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
@@ -13,7 +14,11 @@ class TenantEntityFactoryServiceFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $tenantFactory = new TenantEntityFactory();
+        $tenantFactory   = new TenantEntityFactory();
+        /** @var IdentityFactoryInterface $identityFactory */
+        $identityFactory = $serviceLocator->get('tenantIdentityFactory');
+
+        $tenantFactory->setIdentityFactory($identityFactory);
 
         return $tenantFactory;
     }
